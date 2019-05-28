297a49ff60ebd5559e1efa23ec010889af945ff0
RobertHsu <roberthsu2003@gmail.com>
2019-05-28 14:55:30 +0800

    modifi

--- src/JFrameDemo2.java
+++ src/JFrameDemo2.java
@@ -7,22 +7,34 @@ import javax.swing.border.Border;
 public class JFrameDemo2 {
     private static void constructGUI(){
         JFrame.setDefaultLookAndFeelDecorated(true);
+        //建立JFrame
         JFrame frame = new JFrame();
+        //建立空白邊框的contentPanel
         JPanel contentPanel = new JPanel();
         Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
         contentPanel.setBorder(padding);
         frame.setContentPane(contentPanel);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("JFrame Test");
-        frame.setLayout(new GridLayout(3,2));
-        frame.add(new JLabel("First name:"));
+        //建立frame的Layout
+        frame.setLayout(new GridLayout(3,2,0,10));
+
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameLabel.setHorizontalAlignment(JLabel.RIGHT);
+        frame.add(firstNameLabel);
         frame.add(new JTextField());
-        frame.add(new JLabel("Last Name:"));
+
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        lastNameLabel.setHorizontalAlignment(JLabel.RIGHT);
+        frame.add(lastNameLabel);
+
         frame.add(new JTextField());
+
         frame.add(new JButton("Register"));
 
-        int frameWidth = 200;
-        int frameHeight = 100;
+        int frameWidth = 300;
+        int frameHeight = 150;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setBounds((int)screenSize.getWidth() - frameWidth, 0 , frameWidth, frameHeight);
         frame.setVisible(true);
@@ -30,11 +42,14 @@ public class JFrameDemo2 {
     }
 
     public static void main(String[] args){
+        /*
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 constructGUI();
             }
         });
+        */
+         SwingUtilities.invokeLater(() -> constructGUI());
     }
 }
