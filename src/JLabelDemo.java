2b1a2272e50755bc585afdd13c1adfe2c8a4a720
RobertHsu <roberthsu2003@gmail.com>
2019-06-05 13:42:04 +0800

    modify

--- src/JLabelDemo.java
+++ src/JLabelDemo.java
@@ -1,3 +1,5 @@
+import com.sun.tools.javac.comp.Flow;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -6,18 +8,28 @@ public class JLabelDemo {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame();
         frame.setTitle("JLabel Test");
-        frame.setLayout(new FlowLayout());
+        FlowLayout layout = new FlowLayout();
+        layout.setHgap(20);
+        layout.setVgap(30);
+        frame.setLayout(layout);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         JLabel label1 = new JLabel("First Name");
-        label1.setFont(new Font("Courier New", Font.ITALIC, 12));
+        label1.setFont(new Font("Courier New", Font.ITALIC, 20));
         label1.setForeground(Color.GRAY);
+
         JLabel label2 = new JLabel();
-        label2.setText("<html>Last Name<br><font face='courier new' color=red>(mandatory)</font></html>");
+        label2.setText("<html>Last Name<br><font face='courier new' color='red'>(mandatory)</font></html>");
+
         JLabel label3 = new JLabel();
-        label3.setText("<html>LastName<br><font face=garamond color=red>(mandatory)</font></html>");
-        ImageIcon imageIcon = new ImageIcon("images/heartIcon.png");
-        JLabel label4 = new JLabel(imageIcon);
-        JLabel label5 = new JLabel("Mixed",imageIcon,SwingConstants.RIGHT);
+        label3.setText("<html>LastName<br><font face='garamond' color='red'>(mandatory)</font></html>");
+
+        ImageIcon heartIcon = new ImageIcon("images/heartIcon.png");
+        JLabel label4 = new JLabel(heartIcon);
+
+        ImageIcon addIcon = new ImageIcon("images/add.png");
+
+        JLabel label5 = new JLabel("購物車", addIcon, SwingConstants.RIGHT);
         frame.add(label1);
         frame.add(label2);
         frame.add(label3);
@@ -29,6 +41,7 @@ public class JLabelDemo {
     }
 
     public static void main(String[] args){
-        constructGUI();
+
+        SwingUtilities.invokeLater(() -> constructGUI());
     }
 }
