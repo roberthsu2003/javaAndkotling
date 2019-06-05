497bff920aa10121031bf34c0b4554c0f416cba5
RobertHsu <roberthsu2003@gmail.com>
2019-06-05 12:50:42 +0800

    modify

--- src/JFrameDemo2.kt
+++ src/JFrameDemo2.kt
@@ -1,22 +1,38 @@
+package FrameDemo1
 import java.awt.GridLayout
-import java.awt.Label
 import java.awt.Toolkit
 import javax.swing.*
 
-fun constructGUI2(){
+fun constructGUI(){
     JFrame.setDefaultLookAndFeelDecorated(true)
     val frame = JFrame()
+    //建立空白邊框的contentPanel
+    val contentPanel = JPanel()
+    val padding = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+    contentPanel.border = padding
+    //視窗關閉,JVM同時關閉
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     frame.title = "JFrame Test"
-    frame.layout = GridLayout(3,2)
-    frame.add(Label("First name:"))
+    frame.layout = GridLayout(3, 2, 0, 10)
+
+    //建立FirstNameLabel
+    val firstNameLabel = JLabel("First Name:")
+    firstNameLabel.horizontalAlignment = JLabel.RIGHT
+    frame.add(firstNameLabel)
+
     frame.add(JTextField())
-    frame.add(JLabel("Last Name:"))
+
+    //建立LastNameLabel
+    val lastNameLabel = JLabel("Last Name")
+    lastNameLabel.horizontalAlignment = JLabel.RIGHT
+    frame.add(lastNameLabel)
+
     frame.add(JTextField())
+
     frame.add(JButton("Register"))
 
-    val frameWidth = 200
-    val frameHeight = 100
+    val frameWidth = 300
+    val frameHeight = 150
     val screenSize = Toolkit.getDefaultToolkit().screenSize
     frame.setBounds(screenSize.width - (frameWidth+30), 0, frameWidth, frameHeight )
     frame.isVisible = true
@@ -24,6 +40,6 @@ fun constructGUI2(){
 }
 fun main(){
     SwingUtilities.invokeLater{
-        constructGUI2()
+        constructGUI()
     }
 }
