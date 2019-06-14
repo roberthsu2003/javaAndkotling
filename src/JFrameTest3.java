3d8dfa6edf2b61bab3e453908dfccb84f4f2a858
徐國堂 <roberthus2003@gmail.com>
2019-06-14 17:35:00 +0800

    modify

--- src/JFrameTest3.java
+++ src/JFrameTest3.java
@@ -37,11 +37,6 @@ public class JFrameTest3 {
     }
 
     public static void main(String[] args){
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                constructGUI();
-            }
-        });
+        SwingUtilities.invokeLater(()->constructGUI());
     }
 }
